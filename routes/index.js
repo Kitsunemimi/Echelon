f26f1a03b414dd7054245e216ce37261edb13174
Kitsunemimi <kitsunemimi@live.ca>
2015-11-29 19:48:56 -0500

    Did testing with routing and started some views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
+/* Other general routes */
+
+// Login
+router.get('/login', function(req, res, next) {
+	res.render('login', { title: 'Express' });
+});
+
+// Register
+router.get('/register', function(req, res, next) {
+	res.render('signup', { title: 'Express' });
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	// not sure if we need a listing for all users, so will probably kill this
+	// block
 	res.send('respond with a resource');
+});
+
+// Get specific user profile
+router.get('/:id', function(req, res, next) {
+	res.render('profile', {title: 'Express', id: req.params.id});
 });
 
 module.exports = router;
