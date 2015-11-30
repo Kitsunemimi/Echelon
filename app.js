c3b4f68f399e8e5fbb7ae61d10708f985fe6a668
Kitsunemimi <kitsunemimi@live.ca>
2015-11-30 01:44:05 -0500

    Added some login functionality
    also changed cookie-parser to Mozilla client-sessions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var session = require('client-sessions');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -20,7 +20,12 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(session({
+  cookieName: 'session',
+  secret: 'himitsu',
+  duration: 24 * 60 * 60 * 1000,
+  activeDuration: 24 * 60 * 60 * 1000,
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
