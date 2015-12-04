edde6bcbbd33ed7280c20ebfb3c60a7f237089c1
FionaLo <FionaLo@users.noreply.github.com>
2015-12-04 15:23:52 -0500

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -1,22 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-function searchMain(query){
-	Listing.find({ $text: {$search: query}});
-}
-*/
-
-// Get query (how does the link change?)
-router.get('/search?q=/:query', function(req, res) {
+// Get query
+router.get('/', function(req, res, next) {
+	var query = req.query['q'];
+	res.status(200).send(query);
+	
 	//searches listings to match query
 	Listing.find({ $text: {$search: query}}, function (err, listings){
 		if (err){
-			console.log("Error in finding listing: router.get('/search?q=???')");
+			console.log("Error in finding listing: router.get('/search')");
 			return -1;
 		}
-	res.send(listings);
 	});
 });
 
+module.exports = router;
 
