ce7878f70f039fce4410b29cd563a2eec76b756e
Kitsunemimi <Kitsunemimi@users.noreply.github.com>
2015-12-05 16:35:09 -0500

    Fix merge fail

--- app.js
+++ app.js
@@ -117,10 +117,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/listings', listings);
-app.use('/search', search);
+app.use('/', auth, routes);
+app.use('/users', auth, users);
+app.use('/listings', auth, listings);
+app.use('/search', auth, search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
