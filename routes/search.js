533540258b39ea759eef9c137c692f004a94f178
Kitsunemimi <kitsunemimi@live.ca>
2015-12-06 17:17:11 -0500

    Implemented search

--- routes/search.js
+++ routes/search.js
@@ -3,15 +3,50 @@ var router = express.Router();
 
 // Get query
 router.get('/', function(req, res, next) {
-	var query = req.query['q'];
-	res.status(200).send(query);
+	var query = req.query.q.split(' ');
+	var matches = [];
 	
-	//searches listings to match query
-	Listing.find({ $text: {$search: query}}, function (err, listings){
-		if (err){
-			console.log("Error in finding listing: router.get('/search')");
-			return -1;
+	Listing.find({}, function(err, listings) {
+		if(err) {
+			console.log("Error occurred in search.");
+			return;
 		}
+		
+		for(i = 0; i < listings.length; i++) {
+			// iterate through all listings
+			var titleWords = listings[i].title.split(' ');
+			var tagWords = listings[i].tags;
+			for(j = 0; j < query.length; j++) {
+				// iterate through each query word
+				var found = false;
+				
+				for(k = 0; k < titleWords.length; k++) {
+					// Check against each title word
+					if(query[j].toLowerCase() == titleWords[k].toLowerCase()) {
+						// Found a match
+						matches.push(listings[i]);
+						found = true;
+						break;
+					}
+				}
+				if(found) {
+					continue;
+				}
+				
+				for(k = 0; k < tagWords.length; k++) {
+					// Check against each tag word
+					if(query[j].toLowerCase() == tagWords[k].toLowerCase()) {
+						// Found a match
+						matches.push(listings[i]);
+						found = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		res.locals.listings = matches;
+		res.render('search', {title: "Search - KiBay", query: query.join(' ')});
 	});
 });
 
